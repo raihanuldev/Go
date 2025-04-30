520444e5495c02289071bfa87b6012029ab10922
raihanuldev <rihanthedev@gmail.com>
2025-04-30 23:09:22 +0600

    defrent c++

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 )
-
+const PI = 3.14
 func main() {
+	//loop
+	for i:=0; i < 5; i++ {
+		fmt.Println(i)
+	  }
 
+
+	// Go Constant must be assign when it deaclre
+	const fatherName string  = "John dhon\n"
+	print("Father Name of constrant: ",fatherName)
 	// create a varibale 
 	var student1 string = "Kamrul Hasan\n"
 	var a,b,c,d int = 12,13,13,14 // declare multiple varibale
