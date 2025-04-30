1b0135aa026aab96b99cd94b949cd243f71b074d
raihanuldev <rihanthedev@gmail.com>
2025-04-30 23:02:12 +0600

    refactor: reorganize main function and improve variable declarations

--- main.go
+++ main.go
@@ -5,6 +5,15 @@ import (
 )
 
 func main() {
+
+	// create a varibale 
+	var student1 string = "Kamrul Hasan\n"
+	var a,b,c,d int = 12,13,13,14 // declare multiple varibale
+	print("a: => ",a,"\n")
+	print("b: => ",b,"\n")
+	print("c: => ",c,"\n")
+	print("d: => ",d,"\n")
+	print(student1)
 	fmt.Println("Hello World")
 	fmt.Print("Hello Go ")
 	fmt.Print("Today is Very Intersesting Day for me beacuse iam frist exploreing GoLang")
